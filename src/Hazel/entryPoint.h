123c190c5fae384476186fa2583f0ff14e361ec9
Nanisha430 <542297096@qq.com>
2023-12-27 10:28:17 +0800

    add spdlog

--- src/Hazel/entryPoint.h
+++ src/Hazel/entryPoint.h
@@ -1,13 +1,14 @@
 #pragma once
-#include "stdio.h"
-
+#include "Log.h"
 #ifdef  HZ_PLATFORM_WINDOWS 
 
 extern Hazel::Application* Hazel::CreateApplication();
 
 
 int main(int argc, char** argv) {
-	printf("welcome to the hazel engine \n");
+	Hazel::Log::Init();
+	HZ_CORE_WARN("success Initialized");
+	HZ_CLIENT_INFO("hello");
 	auto app =  Hazel::CreateApplication();
 	app->run();
 	delete app;
