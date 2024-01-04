ea650824f3c3469a6ef3d718a40bb6d929f2c309
Nanisha430 <542297096@qq.com>
2024-01-04 14:44:20 +0800

    add input

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -2,6 +2,7 @@
 #include "Application.h"
 
 #include "Hazel/Log.h"
+#include "Hazel/Input.h"
 
 #include <glad/glad.h>
 
@@ -59,6 +60,8 @@ namespace Hazel {
 
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
+			auto [x, y] = Input::GetMousePosition();
+			HZ_CORE_TRACE("{0}, {1}", x, y);
 
 			m_Window->OnUpdate();
 		}
